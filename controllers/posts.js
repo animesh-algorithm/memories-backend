b39bb2b1840bb5debf13a6e778f3f5f11d835977
Animesh Sharma <animesharma3@gmail.com>
2021-06-21 16:49:02 +0530

    Done with CRUD

--- controllers/posts.js
+++ controllers/posts.js
@@ -35,3 +35,22 @@ export const updatePost = async (req, res) => {
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true })
     res.json(updatedPost)
 }
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Post with the id')
+
+    await PostMessage.findByIdAndRemove(id)
+
+    res.json({ message: 'Post Deleted Successfully' })
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No Post with the id')
+
+    const post = await PostMessage.findById(id)
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true })
+    res.json(updatedPost)
+}
